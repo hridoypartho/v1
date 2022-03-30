dde907474b7b096dc5168f70637c901fad4ab72b
Partho Halder <devpartho@Parthos-MacBook-Pro.local>
2022-03-30 19:01:22 +0600

    contact redesign

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,6 +7,7 @@ const Contact = () => {
   return (
     <section className='section contact center' id='contact'>
       <h2 className='section__title'>Contact</h2>
+      <p className='section__text'>Although I’m not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!</p>
       <a href={`mailto:${contact.email}`}>
         <span type='button' className='btn btn--outline'>
           Email me
